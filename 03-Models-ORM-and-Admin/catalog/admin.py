92e27733bdcf387e07f0c226c26a2b3995e72b10
Dan Rodney <dan@nobledesktop.com>
2025-12-03 12:42:43 -0500

    added comments to AuthorAdmin

--- 03-Models-ORM-and-Admin/catalog/admin.py
+++ 03-Models-ORM-and-Admin/catalog/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Author, Genre, Book
 
 # Register your models here.
+
+# these other classes are allowing us to 
+# control the Models from the Admin Page
+# we do that by using ModelAdmin 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
+    # for managing the Author table in the Admin page 
     list_display = ("first_name", "last_name", "birth_year")
     search_fields = ("first_name", "last_name")
 
