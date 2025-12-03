ccd073bae5d19ad90a1f63c09479d8958868febc
Dan Rodney <dan@nobledesktop.com>
2025-12-03 11:04:18 -0500

    Added home pages, updated routes

--- 01-Django-Foundations/homepage/views.py
+++ 01-Django-Foundations/homepage/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import render
+
+
+# each function is a view. 
+
 def home(request):
-    # return HttpResponse("<h1>Welcome to my portfolio!</h1><p>This is served by Django.</p>")
     context = {
-        "name": "Kash",
-        "interests": ["Coding", "Fitness", "Gaming", "Teaching"]
+        'page_title': 'Home',
+        'intro': 'Welcome to my portfolio! Here are some selected projects.'
     }
-    return render(request, "homepage/home.html", context)
+    return render(request, 'homepage/home.html', context)
+
+
+def about(request):
+    context = {
+        'page_title': 'About',
+        'bio': 'I am a developer who loves Python and web development.'
+    }
+    return render(request, 'homepage/about.html', context)
+
+
+def projects(request):
+    projects_data = [
+        {'name': 'Weather App', 'tech': 'Django, API', 'desc': 'A small weather dashboard.'},
+        {'name': 'Task Manager', 'tech': 'Django, JS', 'desc': 'A to-do CRUD app.'},
+        {'name': 'Portfolio', 'tech': 'HTML, Django', 'desc': 'A personal site.'},
+    ]
+    context = {
+        'page_title': 'Projects',
+        'projects': projects_data
+    }
+    return render(request, 'homepage/projects.html', context)
