8b180ec68030873a001c200973884f30c4da155b
Dan Rodney <dan@nobledesktop.com>
2025-12-04 13:01:19 -0500

    addind exercise text in models

--- travel_project/trips/models.py
+++ travel_project/trips/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+# Exercise: Making A Trip Model: (What goes into making a trip, Include a Image for uploading. )
+
+# Requirements:include an image field and any other fields you see fit! 
