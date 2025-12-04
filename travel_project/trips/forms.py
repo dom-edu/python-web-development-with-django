79e9c61359527bb7d1adfbaddbe66a3f8f78ce9a
Dan Rodney <dan@nobledesktop.com>
2025-12-04 16:30:56 -0500

    added date selection to the form in /new

--- travel_project/trips/forms.py
+++ travel_project/trips/forms.py
@@ -2,6 +2,30 @@ from django import forms
 from .models import Trip 
 
 class TripForm(forms.ModelForm):
+
+    start_date = forms.DateField(
+        # this will add a calendar to select from
+        widget=forms.DateInput(
+            attrs={
+            'type': 'date',
+            'class':'form-control',
+            'placeholder':'Select a Date'
+            }
+        )
+    )
+
+    end_date = forms.DateField(
+        # this will add a calendar to select from
+        widget=forms.DateInput(
+            attrs={
+            'type': 'date',
+            'class':'form-control',
+            'placeholder':'Select a Date'
+            }
+        )
+    )
+
+
     class Meta:
         model = Trip
         fields = ['title',
