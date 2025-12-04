3977b1105407754398880a61b260fc798178640b
Dan Rodney <dan@nobledesktop.com>
2025-12-04 14:10:57 -0500

    implemented trip_list view, added a base.html, trip_list.html , mapped the view to the url trip_list

---
+++ travel_project/trips/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views 
+
+
+# mapped the view to the url trip_list
+urlpatterns = [
+    path('', views.trip_list, name='trip_list')
+]
--- travel_project/trips/views.py
+++ travel_project/trips/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Trip
 
 # Create your views here.
+
+# things we would want to do/see in our app
+# 1. see the list of trips -> def trip_list
+# 2. create a new trip -> def trip_create 
+
+# list trips
+def trip_list(request):
+    # query model
+    trips = Trip.objects.all()
+    # get pass in results for html rendering
+    context = {'trips': trips}
+    return render(request, 'trips/trip_list.html', context)
+
+
+def trip_create(request):
+    pass
